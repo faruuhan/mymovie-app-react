e79ef0a9bfeaa605a1897a09c3989fd00fee95b9
farhan <farhnn.r@gmail.com>
2022-03-23 22:26:26 +0700

    Axios

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import NowPlaying from "./pages/NowPlaying";
+import ListFav from "./pages/ListFav";
 
 ReactDOM.render(
   <React.StrictMode>
