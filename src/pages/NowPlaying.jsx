6bfd08e07d868f6c36a4f3abc5b839e844870a8a
farhan <farhnn.r@gmail.com>
2022-03-23 22:24:41 +0700

    Now Playing Movie with API, btn Favorite

--- src/pages/NowPlaying.jsx
+++ src/pages/NowPlaying.jsx
@@ -1,54 +1,46 @@
 import React, { Component } from "react";
 import Navbar from "../components/Navbar";
 import Container from "../components/Container";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class NowPlaying extends Component {
   constructor(props) {
     super();
     this.state = {
-      data: [
-        {
-          id: 1,
-          title: "The Batman 2022",
-          rate: "8.2",
-          genre: "Action, Crime, Drama",
-          sinopsis: "When the Riddler, a sadistic serial killer, begins murdering key political figures in Gotham, Batman is forced to investigate the city's hidden corruption and question his family's involvement.",
-          image_link: "https://m.media-amazon.com/images/M/MV5BOGE2NWUwMDItMjA4Yi00N2Y3LWJjMzEtMDJjZTMzZTdlZGE5XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg",
-        },
-        {
-          id: 2,
-          title: "The Adam Project",
-          rate: "6.8",
-          genre: "Action, Adventure, Comedy",
-          sinopsis: "After accidentally crash-landing in 2022, time-traveling fighter pilot Adam Reed teams up with his 12-year-old self for a mission to save the future.",
-          image_link: "https://m.media-amazon.com/images/M/MV5BOWM0YWMwMDQtMjE5NS00ZTIwLWE1NWEtODViMWZjMWI2OTU3XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg",
-        },
-        {
-          id: 3,
-          title: "Turning Red",
-          rate: "7.1",
-          genre: "Animation, Adventure, Comedy",
-          sinopsis: "A 13-year-old girl named Meilin turns into a giant red panda whenever she gets too excited.",
-          image_link: "https://m.media-amazon.com/images/M/MV5BNjY0MGEzZmQtZWMxNi00MWVhLWI4NWEtYjQ0MDkyYTJhMDU0XkEyXkFqcGdeQXVyODc0OTEyNDU@._V1_FMjpg_UX1000_.jpg",
-        },
-        {
-          id: 4,
-          title: "The Kashmir Files",
-          rate: "8.3",
-          genre: "Drama, History, Thriller",
-          sinopsis: "The Kashmir Files' is a story, based on video interviews of the first generation victims of the Genocide of Kashmiri Pandit Community In 1990.",
-          image_link: "https://m.media-amazon.com/images/M/MV5BNmUxYjA0NDYtM2UwNS00MzgyLTkxZTktNTFiZGU3MzIxNTI2XkEyXkFqcGdeQXVyOTE2NDU1NDM@._V1_.jpg",
-        },
-      ],
+      data: [],
+      isReady: false,
     };
   }
 
-  async handleClick(item) {
+  async componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API}&language=en-US&page=1`)
+      .then((response) => {
+        this.setState({ data: response.data.results, isReady: true });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  async addToFav(item) {
     let getLocal = JSON.parse(localStorage.getItem("data"));
     if (getLocal) {
-      getLocal.push(item);
-      localStorage.setItem("data", JSON.stringify(getLocal));
+      let findItem = getLocal.findIndex((i) => item.id === i.id);
+      if (findItem != -1) {
+        let newItem = getLocal;
+        getLocal.splice(findItem, findItem + 1);
+        localStorage.removeItem("data");
+        localStorage.setItem("data", JSON.stringify(newItem));
+      } else {
+        getLocal.push(item);
+        localStorage.setItem("data", JSON.stringify(getLocal));
+      }
     } else {
       localStorage.setItem("data", JSON.stringify([item]));
     }
@@ -58,23 +50,20 @@ export default class NowPlaying extends Component {
     return (
       <div>
         <Navbar label="MyMovies" navpage1="Home" navpage2="Favorite" />
-        <h4 className="text-center">Now Playing</h4>
-        <Container className="pt-5">
-          <div className="row justify-content-sm-center flex-wrap gap-3">
+        <div className="container-fluid">
+          <h4 className="text-center py-4">NOW PLAYING</h4>
+          <div className="row d-flex flex-wrap gap-3 justify-content-center">
             {this.state.data.map((item) => {
               return (
-                <div key={item.id} className="card pb-1" style={{ width: "18rem" }}>
-                  <img src={item.image_link} className="card-img-top" alt="..." />
+                <div key={item.id} className="card" style={{ width: "18rem" }}>
+                  <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} className="card-img-top" alt="..." />
                   <div className="card-body">
                     <h5 className="card-title">{item.title}</h5>
-                    <p className="fst-italic">{item.genre}</p>
-                    <p className="card-text">{item.sinopsis}</p>
+                    <p className="card-text">Original Title : {item.original_title}.</p>
+                    <p className="card-text">Release Date : {item.release_date}.</p>
                   </div>
-                  <div className="d-flex justify-content-between">
-                    <a href="#" className="btn btn-success">
-                      Detail
-                    </a>
-                    <button className="btn btn-danger" onClick={() => this.handleClick(item)}>
+                  <div className="d-flex justify-content-end mb-2">
+                    <button className="btn btn-danger" onClick={() => this.addToFav(item)}>
                       Favorite
                     </button>
                   </div>
@@ -82,7 +71,7 @@ export default class NowPlaying extends Component {
               );
             })}
           </div>
-        </Container>
+        </div>
       </div>
     );
   }
